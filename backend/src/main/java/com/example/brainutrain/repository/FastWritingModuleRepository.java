055786ed04f003e367eb30273e3844c929a1a88a
Przemek-Gosik <pregosik322@gmail.com>
2022-11-30 00:02:39 +0100

    FastWritingModuleRepository

---
+++ backend/src/main/java/com/example/brainutrain/repository/FastWritingModuleRepository.java
@@ -0,0 +1,7 @@
+package com.example.brainutrain.repository;
+
+import com.example.brainutrain.model.FastWritingModule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FastWritingModuleRepository extends JpaRepository<FastWritingModule,Long> {
+}
