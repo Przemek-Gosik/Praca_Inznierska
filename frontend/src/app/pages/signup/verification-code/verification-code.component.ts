442ca30a55649500b3d5b5443f9bdc7659ff10b7
DG <dg@gmail.com>
2022-12-11 02:56:43 +0100

    FRONT: Verification Form

--- frontend/src/app/pages/signup/verification-code/verification-code.component.ts
+++ frontend/src/app/pages/signup/verification-code/verification-code.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { VerificationCode } from 'src/app/models/verification-code';
+import { RegistrationService } from 'src/app/services/registration.service';
 
 @Component({
   selector: 'app-verification-code',
@@ -7,7 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerificationCodeComponent implements OnInit {
 
-  constructor() { }
+  title: string = "Weryfikacja konta"
+  instruction: string = "Aby aktywować konto należy wpisać w poniższe pole kod weryfikacyjny otrzymany na adres e-mail (wskazany podczas rejestracji)."
+
+  verificationCode: Partial<VerificationCode> = {};  
+  
+  constructor(private registrationService: RegistrationService,
+    private router: Router) { }
+
+  verify() {
+    console.log(this.verificationCode)
+     this.registrationService.verifyUser(this.verificationCode as VerificationCode).subscribe(
+       result => console.log(result),
+       error => console.log(error),
+   )
+   this.router.navigate(['/']);
+  }
 
   ngOnInit(): void {
   }
--- frontend/src/app/services/registration.service.ts
+++ frontend/src/app/services/registration.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
 import { User } from '../models/user';
+import { VerificationCode } from '../models/verification-code';
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrationService {
 
-  apiUrl = 'http://localhost:8080/api/auth/register';
+  apiUrl = 'http://localhost:8080/api/auth/';
   //deklaracja tokena
 
   // user: User = {email: '', is_email_confirmed: true, login: '', password: ''};
@@ -18,7 +19,13 @@ export class RegistrationService {
 
   registerUser(user: User):Observable<Object>{
     console.log(user);
-    return this.http.post(`${this.apiUrl}`,user).
+    return this.http.post(`${this.apiUrl}`+'register',user).
+    pipe(tap(console.log));
+  }
+
+  verifyUser(code: VerificationCode):Observable<Object>{
+    console.log(code);
+    return this.http.patch(`${this.apiUrl}`+'confirmEmail',code).
     pipe(tap(console.log));
   }
 
