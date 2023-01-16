97d35a6aa25985607eb6683ff6a75156c873c7e7
DG <dg@gmail.com>
2023-01-17 00:23:46 +0100

    FRONT: Contact 1

--- frontend/src/app/pages/forgot-password/forgot-password.component.ts
+++ frontend/src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { forgotPassword } from 'src/app/models/forgot-password-model';
 import { VerificationCode } from 'src/app/models/verification-code';
+import { ForgotPasswordService } from 'src/app/services/forgot-password.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,7 +10,7 @@ import { VerificationCode } from 'src/app/models/verification-code';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private forgotPasswordService: ForgotPasswordService) { }
 
   forgotPassword: Partial<forgotPassword> = {};
   verificationCode: Partial<VerificationCode> = {};
@@ -17,7 +18,27 @@ export class ForgotPasswordComponent implements OnInit {
   errorResponse: string = "";
   verificationFailed: boolean = false;
   forgotPasswordFailed: boolean = false;
+  newPassword: any = "";
+  email: any = "";
+  newPass: string = "";
 
   ngOnInit() {
   }
+
+  sendPasswordRecoveryCode(){
+    this.forgotPasswordService.sendPasswordRecoveryCode(this.forgotPassword as forgotPassword).subscribe((res:any) => {
+      this.email = this.forgotPassword.email;
+    })
+  }
+
+  sendNewPassword(){
+    this.forgotPasswordService.sendNewPassword(this.email as forgotPassword, this.verificationCode as VerificationCode).subscribe((res:any)=> {
+      this.newPassword = res;
+      // this.newPass = JSON.stringify(this.newPassword);
+      this.newPass = this.newPassword.value;
+      console.log(this.newPassword);
+    })
+  }
+
+
 }
