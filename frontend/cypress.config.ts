5a77bc92b16e49d0abacef82cb437220c62dab65
Przemek-Gosik <pregosik322@gmail.com>
2024-11-17 20:01:59 +0100

    Reseting database before every cypress test

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-
     },
     baseUrl: 'http://localhost:4200',
+    env: {
+      backendBaseUrl: 'http://127.0.0.1:8080'
+    },
+    chromeWebSecurity: false,
   },
 });
--- frontend/cypress/support/e2e.ts
+++ frontend/cypress/support/e2e.ts
@@ -16,5 +16,10 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+
+beforeEach(()=>{
+    cy.resetDB();
+});
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
